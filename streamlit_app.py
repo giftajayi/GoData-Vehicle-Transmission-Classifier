01f02d5017967fa0eee40f427887151f2d4253ae
giftajayi <162841212+giftajayi@users.noreply.github.com>
2024-12-02 00:11:18 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,16 @@
 import streamlit as st
+import pandas as pd
+#import matplotlib.pyplot as plt
+#import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.preprocessing import LabelEncoder
+import numpy as np
+import joblib
+import pandas as pd
 
-st.title('🎈 App Name')
+# Title of the App
+st.title("🚗 Vehicle Transmission Classifier")import streamlit as st
 
-st.write('Hello world!')
+
