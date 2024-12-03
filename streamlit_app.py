5ffc134115ba069ff7d616978e4221a562444234
giftajayi <162841212+giftajayi@users.noreply.github.com>
2024-12-03 11:56:00 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,9 +66,12 @@ elif section == "Dataset Overview":
     else:
         st.warning("Main columns not found in the dataset.")
 
+    st.write("### General Information:")
+    st.write(merged_df.info())
+    
     st.write("### General Statistics:")
     st.write(merged_df.describe())
-
+    
 # Visualization Section
 # Visualization Section
 elif section == "Data Visualization":
