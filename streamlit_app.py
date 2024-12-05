19adb562dc2c94370b7ba9cef4012f3006c5f8e1
giftajayi <162841212+giftajayi@users.noreply.github.com>
2024-12-04 19:54:31 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
-    import streamlit as st
+import streamlit as st
 import pandas as pd
 from sklearn.model_selection import train_test_split, GridSearchCV
 from sklearn.ensemble import RandomForestClassifier
